f18b4407a3bd560c63367bcee5c55b24c6bff50f
Sandra Neukirch <35298229+megami13@users.noreply.github.com>
2018-07-25 17:01:38 +0200

    Initial commit

---
+++ Assets/Scripts/ButtonMenu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonMenu : MonoBehaviour {
+
+    public generalInfo generalInfo;
+
+    public Button butonStart, buttonScore, buttonQuit;
+
+    private void Awake()
+    {
+        generalInfo = (generalInfo)FindObjectOfType(typeof(generalInfo));
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("place_ships");
+    }
+
+    public void viewScore()
+    {
+        Debug.Log(generalInfo.getScore());
+        generalInfo.ChangeScore(5);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SetGeneralInfoReference(generalInfo info)
+    {
+        generalInfo = info;
+    }
+}
---
+++ Assets/Scripts/ScoreMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreMenu : MonoBehaviour {
+
+    public generalInfo generalInfo;
+
+    internal void SetGeneralInfoReference(generalInfo info)
+    {
+        generalInfo = info;
+    }
+}
